8b8a40b2f92e47e7aec242ca4422c3a2cf6c8eda
nezam0003 <fhameedk.nezam@gmail.com>
2020-12-15 18:42:32 +0600

    make simple ajax call

---
+++ demo.js
@@ -0,0 +1,27 @@
+const btn = document.getElementById('btn-ajax');
+btn.addEventListener('click', makeRequest);
+
+function makeRequest() {
+    // Create XMLHttpRequest object
+    const xhr = new XMLHttpRequest();
+    // Open XMLHttpRequest function
+    xhr.open('GET', "data.txt", true);
+    // XMLHttpRequest Handler
+    xhr.onreadystatechange = () => {
+        // checking ready state
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            // checking request status
+            if (xhr.status === 200) {
+                console.log(xhr);
+                console.log(xhr.responseText);
+            } else {
+                console.log('Problem Occured');
+            }
+
+        } else {
+            console.log('Could not make request');
+        }
+    }
+    xhr.send();
+
+}
