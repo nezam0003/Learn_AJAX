baad3903748226a58baa5fdf64ba137e1e9764b5
nezam0003 <fhameedk.nezam@gmail.com>
2020-12-16 18:43:46 +0600

    make table fo showing json data in web page

--- demo.js
+++ demo.js
@@ -1,22 +1,22 @@
 const btn = document.getElementById('btn-ajax');
-const userId = document.getElementById('userId');
-const uId = document.getElementById('uId');
-const userTitle = document.getElementById('title');
-const userBody = document.getElementById('body');
+const table = document.getElementById('table');
 btn.addEventListener('click', makeRequest);
 
 function makeRequest() {
     // Create XMLHttpRequest object
     const xhr = new XMLHttpRequest();
     // Open XMLHttpRequest function
-    xhr.open('GET', "https://jsonplaceholder.typicode.com/posts/1", true);
+    xhr.open('GET', "https://jsonplaceholder.typicode.com/posts", true);
     xhr.responseType = "json";
     xhr.onload = () => {
         if (xhr.status === 200) {
-            userId.innerText = xhr.response.userId;
-            uId.innerText = xhr.response.id;
-            userTitle.innerText = xhr.response.title;
-            userBody.innerText = xhr.response.body;
+            console.log(xhr.response);
+            let responseData = xhr.response;
+            for (let i = 0; i < responseData.length; i++) {
+                table.innerHTML += `<tr><td>${responseData[i].userId}</td>
+                <td>${responseData[i].id}</td><td>${responseData[i].title}</td>
+                <td>${responseData[i].body}</td></tr>`;
+            }
         } else {
             console.log('error occured');
         }
