836bd53b042ca39248264fa16d92dd3b07f17d9b
nezam0003 <fhameedk.nezam@gmail.com>
2020-12-16 12:00:26 +0600

    work with JSON file

--- demo.js
+++ demo.js
@@ -1,15 +1,27 @@
 const btn = document.getElementById('btn-ajax');
 const showData = document.getElementById('show-data');
+const nam = document.getElementById('name-id');
+const age = document.getElementById('age-id');
+const position = document.getElementById('position-id');
 btn.addEventListener('click', makeRequest);
 
 function makeRequest() {
     // Create XMLHttpRequest object
     const xhr = new XMLHttpRequest();
     // Open XMLHttpRequest function
-    xhr.open('GET', "data.txt", true);
+    xhr.open('GET', "data.json", true);
+    // xhr.responseType = "json";
     xhr.onload = () => {
         if (xhr.status === 200) {
-            showData.innerText = xhr.responseText;
+            // work with simple text file
+            // showData.innerText = xhr.responseText;
+
+            // work with json data
+            const obj = JSON.parse(xhr.response);
+            nam.innerText = obj.name;
+            age.innerText = obj.age;
+            position.innerText = obj.position;
+
         } else {
             console.log('error occured');
         }
