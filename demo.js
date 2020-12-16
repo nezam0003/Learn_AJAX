b978500be9121df2aba81330ba664b453fe6368f
nezam0003 <fhameedk.nezam@gmail.com>
2020-12-16 11:10:58 +0600

    make some XMLHttpRequest properties test

--- demo.js
+++ demo.js
@@ -2,29 +2,44 @@ document.getElementById('btn-ajax').addEventListener('click', makeRequest);
 
 function makeRequest() {
     const xhr = new XMLHttpRequest();
-    if (xhr.readyState === 0) {
-        console.log('Open method has not called yet');
-    }
+    // if (xhr.readyState === 0) {
+    //     console.log('Open method has not called yet');
+    // }
     xhr.open('GET', 'data.txt', true);
-    if (xhr.readyState === 1) {
-        console.log('Open method has called');
-    }
+    // if (xhr.readyState === 1) {
+    //     console.log('Open method has called');
+    // }
     //*********** onReadyStateChange***********
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === 2) {
-            console.log('Response header');
-        }
-        if (xhr.readyState === 3) {
-            console.log('Loading state...');
-        }
+    // xhr.onreadystatechange = () => {
+    //     if (xhr.readyState === 2) {
+    //         console.log('Response header');
+    //     }
+    //     if (xhr.readyState === 3) {
+    //         console.log('Loading state...');
+    //     }
 
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                console.log(xhr);
-                console.log(xhr.responseText);
-            } else {
-                console.log('problem occured');
-            }
+    //     if (xhr.readyState === 4) {
+    //         if (xhr.status === 200) {
+    //             console.log(xhr);
+    //             console.log(xhr.responseText);
+    //         } else {
+    //             console.log('problem occured');
+    //         }
+    //     }
+    // }
+    // ********** onload state ************
+    xhr.onload = () => {
+        if (xhr.status = 200) {
+            console.log(xhr);
+            console.log(xhr.responseText);
+            console.log(xhr.statusText);
+            console.log(xhr.response);
+            console.log(xhr.withCredentials);
+            console.log(xhr.responseURL);
+            console.log(xhr.getResponseHeader("LAST-Modified"));
+            console.log(xhr.getAllResponseHeaders());
+        } else {
+            console.log('error occured');
         }
     }
     xhr.send();
